8eb9f18c50a19afbf30bbef053d2d033c5d83dcc
shohei81 <syouhei.y.01@gmail.com>
2025-03-14 19:04:59 +0900

    made typescript directory

---
+++ typescript/like-button/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <LikeButton />
+    </>
+  );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+  <span className="likeButton" onClick={handleClick}>
+    ♡ {count}
+    </span>
+  );
+}
+
+export default App;
